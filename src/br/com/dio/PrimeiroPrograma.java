f77109989ca22e02992d8658d20afcc96da650fe
petrucci <petrucci.lucenaplc@gmail.com>
2023-04-24 08:29:31 -0400

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		gato gato = new gato();
+		System.out.println(gato);
+		
+	}
+	
+}
